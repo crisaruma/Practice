a5671dca5b74cf9642e5951063b12c134a5eedb5
gitUser <crisaruma@gmail.com>
2018-07-11 15:47:42 +0900

    練習：SJISとUTF8のテキストデータを、それぞれ読み込み、コンソールへ出力する練習

--- Java/TestProj/src/Lesson/Encode.java
+++ Java/TestProj/src/Lesson/Encode.java
@@ -1,13 +1,21 @@
 package Lesson;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 public class Encode {
     public static void Main(){
         System.out.println("+++ Practice Encode Begin \n");
 
         Encode thisObj = new Encode();
 
+        //  SJISのテキストデータの中身を読み込む
         thisObj.encode( "data/encode-sjis.txt" ,"SJIS");
+        thisObj.encode( "data/encode-sjis.txt" ,"UTF-8");
 
+        //  UTF8のテキストデータの中身を読み込む
+        thisObj.encode( "data/ecnode-utf8.txt" , "SJIS");
         thisObj.encode( "data/ecnode-utf8.txt" , "UTF-8");
 
         System.out.println("\n--- Practice Encode Begin");
@@ -15,14 +23,34 @@ public class Encode {
 
 
     public void encode(String path,String type ) {
-        switch(type ) {
-            case "SJIS":{
-                System.out.println(">>> FileLoad SjisMode");
-            }   break;
+        File hdlFile = new File(path);
 
-            case "UTF-8":{
-                System.out.println(">>> FileLoad Utf-8Mode");
-            }   break;
+        char[] readBuffer = new char[4096];
+
+        FileInputStream ifStream = null;
+        try {
+            ifStream = new FileInputStream(hdlFile);
+            InputStreamReader isReader = new InputStreamReader(ifStream , type);
+            isReader.read(readBuffer);
+            isReader.close();
+            ifStream.close();
+
+            String output = String.valueOf(readBuffer);
+
+            switch(type ) {
+                case "SJIS":{
+                    System.out.println(">>> FileLoad SjisMode");
+                }   break;
+
+                case "UTF-8":{
+                    System.out.println(">>> FileLoad Utf-8Mode");
+                }   break;
+            }
+            System.out.println( output );
+
+        }
+        catch( Exception ex ){
+            System.out.println( ex.getMessage() );
         }
     }
 
