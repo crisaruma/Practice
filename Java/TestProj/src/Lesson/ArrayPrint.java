3ca3143d813d59d140b3b41fe686284e842d4a1e
gitUser <crisaruma@gmail.com>
2018-07-11 16:05:55 +0900

    配列の練習を更新

--- Java/TestProj/src/Lesson/ArrayPrint.java
+++ Java/TestProj/src/Lesson/ArrayPrint.java
@@ -4,15 +4,39 @@ public class ArrayPrint {
     public static void Main(){
         System.out.println("+++ Practice Array Print\n");
 
-        int iArray[] = {0,1,2,3,4,5};
-        String result = "";
-        for( int i : iArray ){
-            result += "LessonPrint : " + i;
-            result += "\n";
-        }
-        System.out.println( result );
+        ArrayPrint thisObj = new ArrayPrint();
+
+        thisObj.printInt();
+
+        thisObj.printByte();
 
         System.out.println("\n--- Practice Array Print");
+    }
+
+    /**
+     * @brief int型配列を宣言、初期化しコンソールに出力する
+     */
+    public void printInt(){
+        int Array[] = {0,1,2,3,4,5};
+        String result = "printInt  : ";
+        for( int val : Array ){
+            result += val;
+            result += ",";
+        }
+        System.out.println( result );
+    }
+
+    /**
+     * @brief byte型配列を宣言、初期化しコンソールに出力する
+     */
+    public void printByte(){
+        byte Array[]={ 0 , 1 , 2 , 0x41 , 0x7F , (byte)(0x80) , -128 , (byte)(-129) };
+        String result = "printByte : ";
+        for( byte val : Array ){
+            result += val;
+            result += ",";
+        }
+        System.out.println( result );
     }
 
 }
